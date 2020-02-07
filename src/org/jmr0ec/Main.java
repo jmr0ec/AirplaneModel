52953c41470d58bb91a9baf8f2667904e29fbf8d
Jacob Riley <JRiley@shelterinsurance.com>
2020-02-07 15:29:56 -0600

    Corrected a bug with the results reporting.

--- src/org/jmr0ec/Main.java
+++ src/org/jmr0ec/Main.java
@@ -2,27 +2,31 @@ package org.jmr0ec;
 
 import org.jmr0ec.model.Airplane;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        int number_of_runs = 10000;
+        int number_of_runs = 1;
         int airplaneSize = 100;
 
         int failed = 0;
         int success = 0;
 
         for(int count = 0; count < number_of_runs; count++){
-            Boolean result = true;
+            Boolean result = false;
             Airplane airplane = new Airplane(airplaneSize);
 
             Integer[] seatList = new Integer[airplaneSize];
             for (int x = 0; x < seatList.length; x++) {
                 seatList[x] = x;
             }
-            //Collections.shuffle(Arrays.asList(seatList));
+            Collections.shuffle(Arrays.asList(seatList));
+            //System.out.println(Arrays.toString(seatList));
 
-            int randomSeatNumber = (int) (Math.random() * airplaneSize);
+            int randomSeatNumber = (int) (Math.random() * (airplaneSize));
 
             airplane.takeSeat(randomSeatNumber);
 
@@ -30,23 +34,24 @@ public class Main {
                 int seatNumber = seatList[i];
 
                 if(airplane.isSeatTaken(seatNumber)){
-                    while(airplane.isSeatTaken(seatNumber)){
-                        seatNumber = (int) (Math.random() * airplaneSize);
+                    int newSeatNumber = (int) (Math.random() * airplaneSize);
+                    while(airplane.isSeatTaken(newSeatNumber)){
+                        newSeatNumber = (int) (Math.random() * airplaneSize);
                     }
-                    airplane.takeSeat(seatNumber);
+                    airplane.takeSeat(newSeatNumber);
                     result = false;
                 }else{
                     airplane.takeSeat(seatNumber);
                     result = true;
                 }
-
-                if(result){
-                    success++;
-                }else{
-                    failed++;
-                }
             }
-            if(count % 100000 == 0){
+
+            if(result){
+                success++;
+            }else{
+                failed++;
+            }
+            if(count % 100000 == 0 && count != 0){
                 System.out.println("Count: " + count);
             }
         }
